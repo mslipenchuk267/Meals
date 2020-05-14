0b25c1e68009e88aa6e965046f23c1a0e01b3953
Matthew Slipenchuk <prontomundo@Matthews-MacBook-Pro.local>
2020-05-14 16:24:34 -0400

    Init

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const CategoriesMealsScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text>The Categories Meals Screen!</Text>
+            <Button title="Go to Meal Details" onPress={() => {
+                props.navigation.navigate('MealDetail');
+            }}/>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default CategoriesMealsScreen;
