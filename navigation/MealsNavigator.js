d9323aef06338f67b20d8ff5ef3d7b7fa62e1afe
Matthew Slipenchuk <prontomundo@Matthews-MacBook-Pro.local>
2020-05-17 19:33:30 -0400

    Added icon tabs

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Platform } from 'react-native';
-
+import { Ionicons } from '@expo/vector-icons'
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen'
 import Colors from '../constants/Colors'
 
 // Create stack navigator
@@ -29,6 +32,27 @@ const MealsNavigator = createStackNavigator({
             headerTintColor:
                 Platform.OS === 'ios' ? Colors.primaryColor : 'white',
         }
-    });
+    }
+);
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {screen: MealsNavigator, navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor}/>;
+        }
+    }},
+    Favorites: {
+        screen: FavoritesScreen, 
+        navigationOptions: {
+        tabBarLabel: 'My Favorites',
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor}/>;
+        }
+    }}
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor
+    }
+});
+
+export default createAppContainer(MealsFavTabNavigator);
