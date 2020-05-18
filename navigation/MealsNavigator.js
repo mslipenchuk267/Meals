1f64526f1db79f9d257c42fa885d63a0a3bfdc3f
Matthew Slipenchuk <prontomundo@Matthews-MacBook-Pro.local>
2020-05-18 02:04:03 -0400

    Styled drawer and added custom fonts where it was missing.

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,7 +3,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
@@ -19,6 +19,12 @@ const defaultStackNavOptions = {
     backgroundColor: 'white',
     headerStyle: {
         backgroundColor: Platform.OS === 'ios' ? 'white' : Colors.primaryColor
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
     },
     headerTintColor:
         Platform.OS === 'ios' ? Colors.primaryColor : 'white',
@@ -51,8 +57,15 @@ const FavNavigator = createStackNavigator({
 
 // Just made this a stack so we get a header
 const FiltersNavigator = createStackNavigator({
-    Filters: FiltersScreen
-})
+    Filters: FiltersScreen,
+},
+    {
+        //navigationOptions: {
+        //    drawerLabel: 'Filter Meals'
+        //},
+        defaultNavigationOptions: defaultStackNavOptions
+    }
+);
 
 const tabScreenConfig = {
     Meals: {
@@ -60,7 +73,8 @@ const tabScreenConfig = {
             tabBarIcon: (tabInfo) => {
                 return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />;
             },
-            tabBarColor: Colors.primaryColor
+            tabBarColor: Colors.primaryColor,
+            tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}} >Meals</Text> : 'Meals'
         }
     },
     Favorites: {
@@ -70,26 +84,42 @@ const tabScreenConfig = {
             tabBarIcon: (tabInfo) => {
                 return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
             },
-            tabBarColor: Colors.accentColor
+            tabBarColor: Colors.accentColor,
+            tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}} >My Favorites</Text> : 'My Favorites'
         }
     }
 }
 
-const MealsFavTabNavigator = Platform.OS === 'android' ?
-    createMaterialBottomTabNavigator(tabScreenConfig, {
+const MealsFavTabNavigator = Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
         activeColor: 'white',
         shifting: true
     })
     : createBottomTabNavigator(tabScreenConfig, {
         tabBarOptions: {
+            labelStyle: {
+                fontFamily: 'open-sans-bold'
+            },
             activeTintColor: Colors.accentColor
         }
     });
 
 
 const MainNavigator = createDrawerNavigator({
-    MealsFavs: MealsFavTabNavigator,
+    MealsFavs: {
+        screen: MealsFavTabNavigator,
+        navigationOptions: {
+            drawerLabel: 'Meals'
+        }
+    },
     Filters: FiltersNavigator
+}, {
+    contentOptions: {
+        activeTintColor: Colors.accentColor,
+        labelStyle: {
+            fontFamily: 'open-sans-bold'
+        }
+    }
 });
 
 export default createAppContainer(MainNavigator);
