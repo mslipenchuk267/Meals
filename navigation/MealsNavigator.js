9611375a436881bbceb767ff83d60b9a07d5a5d4
Matthew Slipenchuk <prontomundo@Matthews-MacBook-Pro.local>
2020-05-17 22:07:32 -0400

    Added custom material tab for android with animation and color changing

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
@@ -35,24 +37,36 @@ const MealsNavigator = createStackNavigator({
     }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-    Meals: {screen: MealsNavigator, navigationOptions: {
-        tabBarIcon: (tabInfo) => {
-            return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor}/>;
+const tabScreenConfig = {
+    Meals: {
+        screen: MealsNavigator, navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />;
+            },
+            tabBarColor: Colors.primaryColor
         }
-    }},
+    },
     Favorites: {
-        screen: FavoritesScreen, 
+        screen: FavoritesScreen,
         navigationOptions: {
-        tabBarLabel: 'My Favorites',
-        tabBarIcon: (tabInfo) => {
-            return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor}/>;
+            tabBarLabel: 'My Favorites',
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
+            },
+            tabBarColor: Colors.accentColor
         }
-    }}
-}, {
-    tabBarOptions: {
-        activeTintColor: Colors.accentColor
     }
-});
+}
+
+const MealsFavTabNavigator = Platform.OS === 'android' ?
+    createMaterialBottomTabNavigator(tabScreenConfig, {
+        activeColor: 'white',
+        shifting: true
+    }) 
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+            activeTintColor: Colors.accentColor
+        }
+    });
 
 export default createAppContainer(MealsFavTabNavigator);
